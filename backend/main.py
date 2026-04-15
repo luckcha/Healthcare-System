a6ad4af3cb087c7daef5ebe3fdf23f24c3423932
Lucky <radheyy1805@gmail.com>
2026-04-15 11:29:15 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://healthcare-system-liart.vercel.app",  # 🔥 तेरा frontend
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
